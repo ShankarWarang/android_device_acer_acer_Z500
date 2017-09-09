0b542c938b1b1fc2bfd9193c0fcc2426227b6774
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-09-09 19:30:54 +0200

    acer_Z500: remove unwanted packages & files

--- device.mk
+++ device.mk
@@ -194,4 +194,31 @@ PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory
 
+# Remove unwanted packages
+PRODUCT_PACKAGES_OVERRIDES += \
+    audio.primary.goldfish \
+    camera.goldfish.jpeg \
+    camera.goldfish \
+    fingerprint.goldfish \
+    fingerprint.ranchu \
+    gps.goldfish \
+    lights.goldfish \
+    power.goldfish \
+    sensors.goldfish \
+    sensors.ranchu \
+    vibrator.goldfish
+
+# Remove unwanted files
+PRODUCT_COPY_FILES_OVERRIDES += \
+    root/fstab.goldfish \
+    root/fstab.ranchu \
+    root/init.goldfish.rc \
+    root/init.ranchu.rc \
+    root/ueventd.goldfish.rc \
+    root/ueventd.ranchu.rc \
+    recovery/root/fstab.goldfish \
+    recovery/root/fstab.ranchu \
+    recovery/root/ueventd.goldfish.rc \
+    recovery/root/ueventd.ranchu.rc
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
